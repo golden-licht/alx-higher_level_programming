b4593cce3616b7fa8259ba92e8e05fb77739a560
golden-licht <luminoonimul@gmail.com>
2024-01-16 13:54:29 +0300

    Check if last digit of a number is greater than 5, zero, or less than 6

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ld = abs(number) % 10  # last digit
+print(f"Last digit of {number:d} is {ld:d} and is", end=' ')
+
+if ld > 5:
+    print("greater than 5")
+elif ld == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
