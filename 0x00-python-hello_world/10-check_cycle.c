55f74c876dce09fdf99db36686b315bc458fe0d0
golden-licht <luminoonimul@gmail.com>
2024-01-21 07:10:21 +0300

    Use floyd's algorithm to check for a cycle in a list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,16 +1,26 @@
 #include "lists.h"
 
+/*
+ * check_cycle - Checks if a linked list has a cycle in it
+ * listint_t: The list to check the cycle for
+ *
+ * Return: 0, if there is no cycle, 1 otherwise.
+ */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *current;
+	listint_t *tortoise = list, *hare = list;
 
-	current = list;
+	if (list == NULL)
+		return (0);
 
-	while (current != NULL)
+	while (hare->next != NULL && (hare->next)->next != NULL)
 	{
-		current = current->next;
-		if (current == list)
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+
+		if (tortoise == hare)
 			return (1);
 	}
-	return 0;
+	return (0);
 }
