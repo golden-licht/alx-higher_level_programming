394a329b13dd8998a7f60621eea14f11c635e352
golden-licht <luminoonimul@gmail.com>
2024-01-26 17:10:43 +0300

    Use list comprehensions instead of map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    for vector in matrix:
-        new_matrix.append(list(map(lambda x: x * x, vector)))
+    new_matrix = [[element**2 for element in row] for row in matrix]
     return new_matrix
