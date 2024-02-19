6a6057657881f53923bcf5debe1d7b40935c9ece
golden-licht <luminoonimul@gmail.com>
2024-02-19 21:08:09 +0300

    Add module doc

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""This module defines the function is_same_class"""
+
+
 def is_same_class(obj, a_class):
     """Return True if an object's exactly an instance of the specified class"""
     if (type(obj) is a_class):
