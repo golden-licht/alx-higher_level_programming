6d97c51ac5109a6bfccf5268f62fcb1d4dd84da8
golden-licht <luminoonimul@gmail.com>
2024-02-20 18:22:32 +0300

    Add doc for argument

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,5 +5,9 @@ import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object"""
+    """Returns the JSON representation of an object
+
+    Args:
+        my_obj (Object): The object
+    """
     return json.dumps(my_obj)
