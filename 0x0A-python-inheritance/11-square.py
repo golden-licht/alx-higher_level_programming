10de20e1a1e7594a34ef7f9da12e41e30d50bbba
golden-licht <luminoonimul@gmail.com>
2024-02-20 05:02:11 +0300

    Add a str() method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This module defines the class Square"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """A class representing a square"""
+    def __init__(self, size):
+        """Initialize a square object"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Implement the str() method"""
+        return f"[Square] {self.__size}/{self.__size}"
