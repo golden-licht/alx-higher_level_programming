113d54090b44a2f03bdf33a50d42b7c9af904be6
golden-licht <luminoonimul@gmail.com>
2024-02-20 18:14:20 +0300

    Add a method that replaces all attributes of a Student instance using attribute value pair dictionary

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""This module contains the definition of a Student class"""
+
+
+class Student:
+    """A class representing a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiate a Student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of a Student instance
+
+        If `attrs` is a list of strings,
+        only attribute names contained in this list are retrieved
+        """
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            new_dict = {x: y for x, y in self.__dict__.items() if x in attrs}
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance using `json`
+
+        Args:
+            json (dict): attribute value pair
+        """
+        for attribute, value in json.items():
+            setattr(self, attribute, value)
