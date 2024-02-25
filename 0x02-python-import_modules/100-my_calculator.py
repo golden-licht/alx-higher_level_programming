dcd97101d12d19a4771730e169d1a250f58b566e
golden-licht <luminoonimul@gmail.com>
2024-02-25 08:50:37 +0300

    Use if else instead of match case for compatibility with earlier versions of python (<3.10)

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,17 +12,16 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    match operator:
-        case '+':
-            result = calculator_1.add(a, b)
-        case '-':
-            result = calculator_1.sub(a, b)
-        case '*':
-            result = calculator_1.mul(a, b)
-        case '/':
-            result = calculator_1.div(a, b)
-        case _:
-            print('Unknown operator. Available operators: +, -, * and /')
-            sys.exit(1)
+    if operator == '+':
+        result = calculator_1.add(a, b)
+    elif operator == '-':
+        result = calculator_1.sub(a, b)
+    elif operator == '*':
+        result = calculator_1.mul(a, b)
+    elif operator == '/':
+        result = calculator_1.div(a, b)
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        sys.exit(1)
 
     print(f'{a} {operator} {b} = {result}')
