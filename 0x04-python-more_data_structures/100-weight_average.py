12f8e15353a90cc1db63bdad1c25a0f74b657cf8
golden-licht <luminoonimul@gmail.com>
2024-02-25 15:32:24 +0300

    Handle empty list input

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,9 @@ def weight_average(my_list=[]):
     A function that returns the weighted average
     of all integers tuple (<score>, <weight>)
     """
-    total_score = sum(map(lambda my_tuple: my_tuple[0] * my_tuple[1], my_list))
-    total_weight = sum(my_list[i][1] for i in range(len(my_list)))
-    average = total_score / total_weight
-    return average
+    if my_list:
+        total_score = sum(map(lambda my_tuple: my_tuple[0] * my_tuple[1], my_list))
+        total_weight = sum(my_list[i][1] for i in range(len(my_list)))
+        average = total_score / total_weight
+        return average
+    return 0
