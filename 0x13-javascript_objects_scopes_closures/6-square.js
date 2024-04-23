bded177461c09bd6fad7f6d07888b987d31dc4d0
golden-licht <luminoonimul@gmail.com>
2024-04-24 00:08:06 +0300

    Define the method charPrint, to print square with given symbol

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin / node
+
+// Write a class Square that defines a square and inherits from Square of 5-square.js:
+
+// You must use the class notation for defining your class and extends
+// Create an instance method called charPrint(c) that prints the rectangle using the character c
+// If c is undefined, use the character X
+
+const _Square = require('./5-square');
+
+class Square extends _Square {
+  charPrint (c) {
+    let character = c;
+    if (character === undefined) character = 'X';
+    for (let i = 0; i < this.height; i++) console.log(character.repeat(this.width));
+  }
+}
+
+module.exports = Square;
