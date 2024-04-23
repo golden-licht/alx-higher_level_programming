29d5d8149dd002984ad294dba9e60c86f03397c1
golden-licht <luminoonimul@gmail.com>
2024-04-23 23:17:22 +0300

    Add instance attributes of width and height

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// A rectangle class with width and heigth instance attributes
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
