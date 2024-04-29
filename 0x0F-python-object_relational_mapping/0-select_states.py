6a899e5b45f59793ded12b50dd8d69c2d61e479e
golden-licht <luminoonimul@gmail.com>
2024-04-29 15:35:41 +0300

    Connect and fetch table values from a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" When run as a script, lists all states from the states table of a database
+that is given as a third argument, the first and second argument
+being username and password to access mysql """
+
+from sys import argv
+from MySQLdb import _mysql
+from MySQLdb.constants import FIELD_TYPE
+
+
+if __name__ == "__main__":
+    username, password, database = [argv[1], argv[2], argv[3]]
+    # to convert mysql types to python types
+    my_conv = {FIELD_TYPE.LONG: int, FIELD_TYPE.VAR_STRING: str}
+
+    db = _mysql.connect(
+        conv=my_conv,
+        host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        database=database)
+    db.query("""SELECT * FROM states
+                    ORDER BY id ASC""")
+    result = db.store_result()
+    # fetch all the rows
+    all_rows = result.fetch_row(maxrows=0)
+
+    for row in all_rows:
+        print(row)
