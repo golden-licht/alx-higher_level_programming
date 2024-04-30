b9e03954415eed8b10a723a1074e9d5ed4de14de
golden-licht <luminoonimul@gmail.com>
2024-04-30 06:42:58 +0300

    Use MySQLdb instead of _mysql and avoid sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,27 +10,24 @@ fourth argument is the state name to be searched.
 
 
 from sys import argv
-from MySQLdb import _mysql
+import MySQLdb
 from MySQLdb.constants import FIELD_TYPE
 
 if __name__ == "__main__":
     username, password, database, state_name = argv[1:]
-    # to convert mysql types to python types
-    my_conv = {FIELD_TYPE.LONG: int, FIELD_TYPE.VAR_STRING: str}
 
-    db = _mysql.connect(
-        conv=my_conv,
+    db = MySQLdb.connect(
         host='localhost',
         port=3306,
         user=username,
         password=password,
         database=database)
-    db.query("""SELECT * FROM states
-                  WHERE BINARY name = '{}'
-                  ORDER BY id ASC""".format(state_name))
-    result = db.store_result()
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+                  WHERE BINARY name = %s
+                  ORDER BY id ASC""", (state_name,))
     # fetch all the rows
-    all_rows = result.fetch_row(maxrows=0)
+    all_rows = cursor.fetchall()
 
     for row in all_rows:
         print(row)
