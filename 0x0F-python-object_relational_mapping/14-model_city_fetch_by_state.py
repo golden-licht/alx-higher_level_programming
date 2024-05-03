e427f9941af19612be0e520774219b56a8389a42
golden-licht <luminoonimul@gmail.com>
2024-05-03 20:15:33 +0300

    Pycodestyle

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
-# Next, write a script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa:
-
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module SQLAlchemy
-#     You must import State and Base from model_state - from model_state import Base, State
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Results must be sorted in ascending order by cities.id
-#     Results must be display as they are in the example below (<state name>: (<city id>) <city name>)
-#     Your code should not be executed when imported
-
 """
 When run as a script, prints all cities with their states from cities table.
 First and second argument should be mysql username and password.
@@ -20,8 +10,8 @@ from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from model_state import State 
-from model_city import City 
+from model_state import State
+from model_city import City
 
 if __name__ == "__main__":
     username, password, database = argv[1:]
@@ -30,7 +20,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State).filter(City.state_id == State.id):
+    for city, state in session.query(
+            City, State).filter(City.state_id == State.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
