5f28b98a13fd664fcc7feba0b1ae1208f6dc0ddc
golden-licht <luminoonimul@gmail.com>
2024-05-03 20:17:10 +0300

    Pycodestyle

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
-# Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City.
-
-#     City class:
-#         inherits from Base (imported from model_state)
-#         links to the MySQL table cities
-#         class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-#         class attribute name that represents a column of a string of 128 characters and can’t be null
-#         class attribute state_id that represents a column of an integer, can’t be null and is a foreign key to states.id
-#     You must use the module SQLAlchemy
-
 
 """This module defines the City class representing cities in the database."""
 
@@ -17,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
+
 
 class City(Base):
     """
