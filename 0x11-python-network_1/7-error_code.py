4b399c247d80ae5b0db14ddc0edb0e31cd80a80d
golden-licht <luminoonimul@gmail.com>
2024-05-11 05:57:01 +0300

    Handle http erros

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""""Handle errors"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    response = requests.get(argv[1])
+    status_code = response.status_code
+    if (status_code >= 400):
+        print('Error code: ', status_code)
+    else:
+        print(response.text)
