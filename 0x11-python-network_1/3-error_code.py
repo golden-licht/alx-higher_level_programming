abfee05b0050cd9059f3e3564665573074cebfeb
golden-licht <luminoonimul@gmail.com>
2024-05-11 05:18:21 +0300

    Handle http error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
             body = response.read()
             print(body.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(e.code)
+        print('Error code:', e.code)
