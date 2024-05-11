496db0f71a9451d3deb19de1a4ab3a37dd247778
golden-licht <luminoonimul@gmail.com>
2024-05-11 05:26:47 +0300

    Http request using teh requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Fetch https://alx-intranet.hbtn.io/status using request package"""
+
+import requests
+
+if __name__ == '__main__':
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    body = response.text
+    print('Body response:')
+    print('\t- type: ', type(body))
+    print('\t- content:', body)
