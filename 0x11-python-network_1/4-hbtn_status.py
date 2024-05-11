f8803d33b74383ed6d432e2c756b3b8438213d7a
golden-licht <luminoonimul@gmail.com>
2024-05-11 05:44:38 +0300

    Fix indentation

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     response = requests.get('https://alx-intranet.hbtn.io/status')
     body = response.text
     print('Body response:')
-    print('\t- type: ', type(body))
+    print('\t- type:', type(body))
     print('\t- content:', body)
