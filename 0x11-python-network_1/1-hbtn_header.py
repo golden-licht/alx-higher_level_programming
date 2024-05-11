0cabafde2d3e32e0b33facb3ea394275e8f38a45
golden-licht <luminoonimul@gmail.com>
2024-05-11 04:17:36 +0300

    Don't run when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,9 @@ X-Request-Id variable found in the header of the response."""
 import urllib.request
 from sys import argv
 
-req = urllib.request.Request(argv[1])
-with urllib.request.urlopen(req) as response:
-    header = response.info()
+if __name__ == '__main__':
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        header = response.info()
 
-print(header.get('X-Request-Id'))
+    print(header.get('X-Request-Id'))
